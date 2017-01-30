a375817379a32e8b877aee034287aff2e4c517f2
Patrick Short <pjsy8c@gmail.com>
2017-01-30 17:56:49 -0600

    added ascii-chan framework

---
+++ main.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+# import urllib
+#
+# from google.appengine.api import users
+from google.appengine.ext import db
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = JINJA_ENVIRONMENT.get_template(template)
+        return t.render(params)
+
+    def render(self,template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(MainHandler):
+    def render_front(self, title="", art="",error=""):
+        arts = db.GqlQuery("SELECT * FROM Art "
+                            "ORDER BY created DESC ")
+
+        self.render("front.html", title=title, art=art, error=error, arts=arts)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get('title')
+        art = self.request.get('art')
+
+        if title and art:
+            a = Art(title=title, art=art)
+            a.put()
+
+            self.redirect("/")
+        else:
+            error = "We need both a title and some artwork!"
+            self.render_front(title, art, error)
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
